c7e5ead76897be02c46fedadfd1aac80abd115fc
Mehdi Rezaei <mehdiraized@gmail.com>
2023-11-17 22:09:55 +0330

    fix href Card

--- src/components/Card/Card.component.tsx
+++ src/components/Card/Card.component.tsx
@@ -1,14 +1,13 @@
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 const Card = ({ name, age }: { name: string; age: number }) => {
 	const t = useTranslations("Card");
 
 	return (
-		<a
-			href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+		<Link
+			href=""
 			className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-			target="_blank"
-			rel="noopener noreferrer"
 		>
 			<h2 className={`mb-3 text-2xl font-semibold capitalize`}>{name}</h2>
 			<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -20,7 +19,7 @@ const Card = ({ name, age }: { name: string; age: number }) => {
 					{age}
 				</span>
 			</p>
-		</a>
+		</Link>
 	);
 };
 
